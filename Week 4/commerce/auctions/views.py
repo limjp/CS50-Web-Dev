586ebbb56f026a51ccb5dfce73ae1066641819bd
Jun Ping <limjp14@gmail.com>
2021-11-13 15:23:29 +0800

    Implemented watchlist, bidding and commenting.

--- Week 4/commerce/auctions/views.py
+++ Week 4/commerce/auctions/views.py
@@ -82,7 +82,13 @@ def create_listing(request):
                 starting_price = form.cleaned_data["starting_price"],
                 creator = user
             )
+            starting_bid = Bid(
+                auction = new_listing,
+                bidder = request.user,
+                price = form.cleaned_data["starting_price"]
+            )
             new_listing.save()
+            starting_bid.save()
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "auctions/make_listing.html", {
@@ -96,22 +102,37 @@ def listing(request, listing_id):
     if request.method == "POST":
         form = BidForm(request.POST)
         if "submit_bid" in request.POST and form.is_valid():
+            price = form.cleaned_data["price"]
             listing = Listing.objects.get(id=listing_id)
-            new_bid = Bid(
-                price = form.cleaned_data["price"],
-                auction = listing,
-                bidder = request.user)
-            new_bid.save()
-            return HttpResponseRedirect(reverse("index"))
+            current_price = Bid.objects.filter(auction = listing).latest("price").price
+            if price > current_price:
+                new_bid = Bid(
+                    price = price,
+                    auction = listing,
+                    bidder = request.user)
+                new_bid.save()
+                messages.success(request, "Bid successfully placed!")
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "current_price": current_price
+                })
+            else:
+                messages.error(request, "Bid cannot be less than current price")
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "current_price": current_price
+                })
         elif "add_watchlist" in request.POST:
-            listing = Listing.objects.get(listing_id)
-            watchlist_item = Listing.objects.create(watchers=request.user.id)
-            listing.watchers.add(watchlist_item)
+            listing = Listing.objects.get(id=listing_id)
+            listing.watchers.add(request.user)
+            listing.save()
             return HttpResponseRedirect(reverse("index"))
         elif "close_auction" in request.POST:
-            listing = Listing.objects.get(listing_id)
+            listing = Listing.objects.get(id=listing_id)
             listing.is_active = False
+            listing.winner = Bid.objects.filter(auction=listing).latest("date_created").bidder
             listing.save()
+            return HttpResponseRedirect(reverse("index"))
         elif not(form.is_valid()):
             return render(request, "auctions/listing.html", {
                 "form": form
@@ -119,7 +140,12 @@ def listing(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
     all_comments = listing.comments.all()
     is_user_logged_in = request.user.is_authenticated
-    is_creator = request.user == listing.creator
+    is_creator = request.user == listing.creator  
+    current_bid = Bid.objects.filter(auction=listing).latest("price")
+    if current_bid is None:
+        current_price = 0.00
+    else:
+        current_price = current_bid.price
     return render(request, "auctions/listing.html", {
         "bid_form": BidForm(),
         "user_id": request.user.id,
@@ -127,8 +153,44 @@ def listing(request, listing_id):
         "listing": listing,
         "is_user_logged_in": is_user_logged_in,
         "is_creator": is_creator,
-        "comment_form": CommentForm()
+        "comment_form": CommentForm(),
+        "current_price": current_price
     })
 
 def create_comment(request, listing_id):
-    pass
+    if request.method == "POST":
+        listing = Listing.objects.get(id=listing_id)
+        all_comments = listing.comments.all()
+        is_user_logged_in = request.user.is_authenticated
+        is_creator = request.user == listing.creator  
+        current_price = Bid.objects.filter(auction=listing).latest("price").price
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = Comment(
+                comment = form.cleaned_data["comment"],
+                author = request.user,
+                commented_on = listing
+            )
+            new_comment.save()
+            all_comments = listing.comments.all()
+            return render(request, "auctions/listing.html", {
+                "bid_form": BidForm(),
+                "user_id": request.user.id,
+                "comments": all_comments,
+                "listing": listing,
+                "is_user_logged_in": is_user_logged_in,
+                "is_creator": is_creator,
+                "comment_form": CommentForm(),
+                "current_price": current_price
+            })
+        else:
+            return render(request, "auctions/listing.html", {
+                "bid_form": BidForm(),
+                "user_id": request.user.id,
+                "comments": all_comments,
+                "listing": listing,
+                "is_user_logged_in": is_user_logged_in,
+                "is_creator": is_creator,
+                "comment_form": CommentForm(),
+                "current_price": current_price
+            })
